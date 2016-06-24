d749bd670be436f73e8158f94c682d48519b678d
soamiba <soamiba@hotmail.com>
2016-06-24 12:44:27 +0800

    inline style add radium for navbar

--- imports/ui/shared/NavBar.jsx
+++ imports/ui/shared/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Tabs, Tab } from 'material-ui/Tabs';
+import Radium from 'radium';
 
 class NavBar extends Component {
   constructor(props) {
@@ -27,12 +28,37 @@ class NavBar extends Component {
   }
 
   render() {
+    let styles = {
+      root: {
+        flexShrink: '0',
+        height: '64px',
+        backgroundColor: '#00bcd4',
+        boxShadow: '0 1px 6px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.24)',
+      },
+      tabs: {
+        width: '390px',
+        position: 'absolute',
+        right: '60px',
+        textTransform: 'uppercase',
+      },
+      tab: {
+        height: '64px',
+        color: '#fff',
+      },
+      inkBar: {
+        height: '4px',
+        marginTop: '-4px',
+      },
+    };
     return (
-      <div>
-        <Tabs value={this.state.tabIndex} onChange={this.handleChange.bind(this)}>
-          <Tab label='Home' value='/' />
-          <Tab label='Sign Up' value='/signup' />
-          <Tab label='Log In' value='/login' />
+      <div style={styles.root}>
+        <Tabs value={this.state.tabIndex} onChange={this.handleChange.bind(this)}
+          style={styles.tabs}
+          inkBarStyle={styles.inkBar}
+          tabItemContainerStyle={{backgroundColor: 'transparent'}}>
+          <Tab label='Home' value='/' style={styles.tab} />
+          <Tab label='Sign Up' value='/signup' style={styles.tab} />
+          <Tab label='Log In' value='/login' style={styles.tab} />
         </Tabs>
       </div>
     );
@@ -43,4 +69,4 @@ NavBar.contextTypes = {
   muiTheme: React.PropTypes.object.isRequired,
   router: React.PropTypes.object.isRequired
 }
-export default NavBar;
+export default Radium(NavBar);
